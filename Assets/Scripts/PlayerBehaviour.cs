46df69d599153032106a91116761d5a4676ffe21
Tyler <phantomxbla@gmail.com>
2021-10-22 22:39:27 -0400

    Added player jump functionality, in the process of implementing slide

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,19 +4,49 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    //Touch touch;
+    //public GameObject player;
     Touch touch;
+
+    Rigidbody2D body;
+    Animator animator;
+
+    Vector2 jumpForce = new Vector2(0, 1000);
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       if (touch.phase == TouchPhase.Moved)
+        if(transform.position.y <= -2.7)
         {
-            Debug.Log("swipe");
-        } 
+            animator.SetBool("isGrounded", true);
+            
+        }
+        else
+        {
+            animator.SetBool("isGrounded", false);
+        }
+        if(Input.touchCount > 0)
+        {
+             touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    body.AddForce(jumpForce);
+                    animator.SetTrigger("JumpTrigger");
+                    Debug.Log("screen pressed");
+                    break;
+
+                case TouchPhase.Ended:
+                    Debug.Log("nothing is touching the screen big man");
+                    break;
+            }
+        }     
     }
 }
