33b7e8b02da0c9d8d645370c4207883ca4911633
Tyler <phantomxbla@gmail.com>
2021-10-24 14:39:02 -0400

    Added ability for game to spawn enemies at random time intervals

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -73,6 +73,14 @@ public class PlayerBehaviour : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "enemy")
+        {
+            lives--;
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -7,15 +7,58 @@ public class SpawnController : MonoBehaviour
     public GameObject Wolf;
     public GameObject Bat;
     public GameObject Coin;
+
+    public GameObject WolfSpawn;
+    public GameObject BatSpawn;
+    public GameObject CoinSpawn;
+
+    float wolfSpawnTimer;
+    float batSpawnTimer;
+    float coinSpawnTimer;
+
+    int batCount;
+    int wolfCount;
+    int coinCount;
+
+    bool canSpawnBat = true;
+    bool canSpawnWolf = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        batSpawnTimer = Random.Range(5.5f, 10.5f);
+        wolfSpawnTimer = Random.Range(4.5f, 9.5f);
+    }
+
+    IEnumerator batCooldown()
+    {
+        if (canSpawnBat)
+        {
+            Instantiate(Bat, BatSpawn.transform);
+            canSpawnBat = false;
+            yield return new WaitForSeconds(batSpawnTimer);
+            canSpawnBat = true;
+
+            batSpawnTimer = Random.Range(5.5f, 10.5f);
+        }
+    }
+
+    IEnumerator wolfCooldown()
+    {
+        if (canSpawnWolf)
+        {
+            Instantiate(Wolf, WolfSpawn.transform);
+            canSpawnWolf = false;
+            yield return new WaitForSeconds(wolfSpawnTimer);
+            canSpawnWolf = true;
+
+            wolfSpawnTimer = Random.Range(4.5f, 9.5f);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        StartCoroutine(batCooldown());
+        StartCoroutine(wolfCooldown());
     }
 }
